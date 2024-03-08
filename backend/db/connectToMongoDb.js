9069374d6548bec3a073ce04dc9ceb4bc22e22d0
anurago9o9 <58781750+anurago9o9@users.noreply.github.com>
2024-03-09 04:31:20 +0530

    third commit

--- backend/db/connectToMongoDb.js
+++ backend/db/connectToMongoDb.js
@@ -1,16 +1,12 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-// Load environment variables
-dotenv.config();
 
 const connectToMongoDB = async () => {
-   try {
-      await mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-      console.log("Connected to MongoDB");
-   } catch (error) {
-      console.log("Error connecting to MongoDB", error.message);
-   }
+	try {
+		await mongoose.connect(process.env.MONGO_DB_URI);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.log("Error connecting to MongoDB", error.message);
+	}
 };
 
 export default connectToMongoDB;
